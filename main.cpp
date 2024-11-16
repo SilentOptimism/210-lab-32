6503af41899bc5218d95811ae221d43687bc0b2f
Joey Dhillon <imjoeydhillon@gmail.com>
2024-11-15 19:08:25 -0800

    commit 3

--- main.cpp
+++ main.cpp
@@ -1,28 +1,53 @@
 #include <iostream>
 #include <chrono>
+#include <deque>
 #include "Car-1.h"
 
 using namespace std;
 using namespace chrono;
 
+deque<Car> vehicles;
+int timeOperations = 0;
+
 int main(int argc, char const *argv[])
 {
+    srand(time(NULL));
+
+
+
+    cout << "Initial queue:" << endl;
+
+    vehicles.push_back(Car());
+    vehicles.back().print();
+
+    vehicles.push_back(Car());
+    vehicles.back().print();
 
     time_point start = high_resolution_clock::now();
     time_point now = high_resolution_clock::now();
-
-    cout << "Initial queue:" << endl;
 
     while(true){
         now = high_resolution_clock::now();
         milliseconds duration = duration_cast<milliseconds>(now-start);
 
-        if(duration.count() % 1000 == 0){
-            cout << duration.count() << endl;
+        if(duration.count() > 1000){
+            cout << "Time " << timeOperations+1 << " Operation: ";
+
+            int event = rand() % 100 + 1;
+
+            if(event <= 45){
+                cout << "Joined the line";
+
+            }
+            else {
+                cout << "Payed their toll";
+
+            }
+
+            start = high_resolution_clock::now();
+            timeOperations++;
         }
-        
-
-
     }
+
     return 0;
 }
