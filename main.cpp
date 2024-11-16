cc391170c7585c1081da355f50f2499433743738
Joey Dhillon <imjoeydhillon@gmail.com>
2024-11-15 18:57:40 -0800

    commit 2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <chrono>
+#include "Car-1.h"
 
 using namespace std;
+using namespace chrono;
 
 int main(int argc, char const *argv[])
 {
-    cout <<  "test" << endl;
+
+    time_point start = high_resolution_clock::now();
+    time_point now = high_resolution_clock::now();
+
+    cout << "Initial queue:" << endl;
+
+    while(true){
+        now = high_resolution_clock::now();
+        milliseconds duration = duration_cast<milliseconds>(now-start);
+
+        if(duration.count() % 1000 == 0){
+            cout << duration.count() << endl;
+        }
+        
+
+
+    }
     return 0;
 }
